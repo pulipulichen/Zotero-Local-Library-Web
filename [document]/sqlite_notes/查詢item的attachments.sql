4a95a029aeed357d50ee824c8afb4ea672fbf74c
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-09-16 17:32:21 +0800

    0916 好，查詢的手段都完成了，然後是...

--- [document]/sqlite_notes/查詢item的attachments.sql
+++ [document]/sqlite_notes/查詢item的attachments.sql
@@ -1,4 +1,9 @@
-select itemAttachments.* 
+select itemAttachments.*, items.key 
 from itemAttachments
+left join items using (itemID)
 where
 itemAttachments.parentItemID = 689  -- itemID 689 = 操作介面設計模式
+
+select *
+from item
+where itemID = 17836
