89be0147ab087b771a37536dea2e92cf8f360e22
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-09-16 17:00:17 +0800

    0916 開始整理一下SQL，找到了

--- [document]/sqlite_notes/查詢item的attachments.sql
+++ [document]/sqlite_notes/查詢item的attachments.sql
@@ -1,9 +1,4 @@
-select items.itemID, fields.fieldID, fieldName, value, itemAttachments.* 
-from items
-left join itemData using(itemID) 
-left join itemDataValues using(valueID)
-left join fields using(fieldID)
-left join itemAttachments using(itemID)
-where itemTypeID = 2
-and (fields.fieldID = 110)
-and items.itemID = 689  -- 操作介面設計模式
+select itemAttachments.* 
+from itemAttachments
+where
+itemAttachments.parentItemID = 689  -- itemID 689 = 操作介面設計模式
