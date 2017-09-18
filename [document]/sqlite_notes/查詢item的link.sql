8478e0ffe05d428d33ff86927fed77e415cb8d01
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-09-18 12:33:54 +0800

    0918 要把sql加進去之前

--- [document]/sqlite_notes/查詢item的link.sql
+++ [document]/sqlite_notes/查詢item的link.sql
@@ -1,19 +1,18 @@
 select 
-itemDataValuesLink.value
+itemAttachments.parentItemID,
+itemLink.value
 from 
 itemAttachments 
-    join itemData as itemDataTitle on itemAttachments.itemID = itemDataTitle.itemID and itemDataTitle.fieldID = 110
-    join itemDataValues as itemDataValuesTitle on itemDataTitle.valueID = itemDataValuesTitle.valueID and value = 'Google Drive'
-    join itemData as itemDataLink on itemAttachments.itemID = itemDataLink.itemID and fieldID = 1
-    join itemDataValues as itemDataValuesLink on itemDataLink.valueID = itemDataValuesLink.valueID
+    join (
+        select items.itemID, value, items.dateModified from items join itemData using (itemID) 
+            join itemDataValues on itemDataValues.valueID = itemData.valueID and itemData.fieldID = 110 
+        ) as itemTitle using (itemID)
+    join (
+        select items.itemID, value from items join itemData using (itemID) 
+            join itemDataValues on itemDataValues.valueID = itemData.valueID and itemData.fieldID = 1 
+        ) as itemLink using (itemID)
 where 
 linkMode = 3
 and parentItemID = 17392
-
--- 25216
-
-select *
-from items join itemData using(itemID) join itemDataValues using(valueID)
-where items.itemID = 25216
-
--- fieldID = 1
+group by parentItemID
+order by dateModified DESC
--- [document]/sqlite_notes/查詢item的title, creators, date.sql
+++ [document]/sqlite_notes/查詢item的title, creators, date.sql
@@ -4,13 +4,22 @@ itemCreators.item_creators AS item_creators,
 substr(itemDate.value, instr(itemDate.value, ' ') + 1) AS item_date,
 itemTitle.dateModified AS item_modified_date,
 itemCover.item_cover_path AS item_cover_path,
-ifnull(item_attachment_count, 0) AS item_attachment_count
+ifnull(item_attachment_count, 0) AS item_attachment_count,
+itemLink.value as item_link
 FROM
 
 (items
 left join itemData using(itemID) 
 left join itemDataValues using(valueID)
-left join fields using(fieldID)) as itemTitle,
+left join fields using(fieldID)) as itemTitle
+
+join 
+(select 
+    count(itemID) as item_attachment_count, 
+    itemAttachments.parentItemID
+    from itemAttachments
+    where contentType = 'application/pdf'
+    group by itemAttachments.parentItemID) as itemAttachmentCount on itemCreators.itemID = itemAttachmentCount.parentItemID,
 
 (items
 left join itemData using(itemID) 
@@ -41,19 +50,30 @@ left join
     group by itemAttachments.parentItemID
     order by dateModified DESC) as itemCover on itemCreators.itemID = itemCover.parentItemID
 
-left join 
+left join
 (select 
-    count(itemID) as item_attachment_count, 
-    itemAttachments.parentItemID
-    from itemAttachments
-    where contentType = 'application/pdf'
-    group by itemAttachments.parentItemID) as itemAttachmentCount on itemCreators.itemID = itemAttachmentCount.parentItemID
+    itemAttachments.parentItemID,
+    itemLink.value
+    from 
+    itemAttachments 
+        join (
+            select items.itemID, value, items.dateModified from items join itemData using (itemID) 
+                join itemDataValues on itemDataValues.valueID = itemData.valueID and itemData.fieldID = 110 
+            ) as itemTitle using (itemID)
+        join (
+            select items.itemID, value from items join itemData using (itemID) 
+                join itemDataValues on itemDataValues.valueID = itemData.valueID and itemData.fieldID = 1 
+            ) as itemLink using (itemID)
+    where 
+    linkMode = 3
+    group by parentItemID
+    order by dateModified DESC) as itemLink on itemLink.parentItemID = itemCreators.itemID
 
 WHERE 
 itemTitle.itemID = itemDate.itemID
 and itemTitle.itemID = itemCreators.itemID
 and itemTitle.fieldID = 110
 and itemDate.fieldID = 14
-and itemTitle.itemID = 1
+and itemTitle.itemID = 17392
 ORDER BY
 itemTitle.dateModified DESC
